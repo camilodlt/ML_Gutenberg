cc9346f324adaee5a57cdf83a190096f61e44d11
Camilo De La Torre <dlt.camilo@gmail.com>
2021-06-11 17:29:39 +0200

    Comment a little

--- R/functions_synthetic_mistakes.R
+++ R/functions_synthetic_mistakes.R
@@ -2,15 +2,21 @@
 ##          Functions for applying synthetic mistakes          ##
 #################################################################
 
-source("R/common.R")
-############
-my_natural<- natural_mistakes(word= "hello", char_modification_prob = c(0,1), # pass all words to the fat finger_decider
+# LIBRARIES ------
+library(spell4french)
+# INIT NATURAL MISTAKES
+  #* This function applies fat_finger mistakes to a word ---
+  # Fat fingerness is a parameter
+  # faat_finger = 0.1 => for each char, prob 10% that it'll be concatenated with its neighbors ("a" with "z" for ex)
+my_natural<- spell4french:::natural_mistakes(word= "", char_modification_prob = c(0,1), # pass all words to the fat finger_decider
                              max_neighbor_size=2,sample_neighbor_prob = c(0.95,0.05),
                              fat_finger=0.1,return_fun = TRUE)
 
-
+  #* Decider for a sentence ---
+  # 65 % chances that the sentence will pass through the decider word function
+  # returns the sentence modified or not
 decider <- function(x, sep= " ",...){
-  rand<-sample(0:1, size = 1, prob = c(0.4, 0.6))
+  rand<-sample(0:1, size = 1, prob = c(0.35, 0.65))
   if(rand==0){
     return(x)
   }
@@ -21,7 +27,11 @@ decider <- function(x, sep= " ",...){
     return(synthetic)
   }
 }
-
+  #* Decider for a each word ---
+  # 30% % chances that the word will pass through the apply_depth_multiple
+  # Exclusively, Natural mistakes are preferred (50 % of the time, if the word is to be modified)
+  # Apply multiple depth accepts multiple transformations. (functions)
+  # returns the word (non modified) or the word modified
 decider_word<- function(x,modification_prob= 0.3,
                         functions=  c(my_natural,"de_split","deletes","transposes","replaces","insertions")){
   rand<-sample(0:1, size = 1, prob = c(1-modification_prob, modification_prob))
@@ -40,36 +50,8 @@ decider_word<- function(x,modification_prob= 0.3,
   }
 }
 
-
-#############
-decider <- function(x, sep= " ",...){
-  rand<-sample(0:1, size = 1, prob = c(0.5, 0.5))
-  if(rand==0){
-    return(x)
-  }
-  else {
-    words<-unlist(strsplit(x, sep))
-    synthetic<-map_chr(words,~ decider_word(.),...)
-    synthetic<- paste0(synthetic, collapse = sep)
-    return(synthetic)
-  }
-}
-
-decider_word<- function(x,prob= 0.3){
-  rand<-sample(0:1, size = 1, prob = c(1-prob, prob))
-  if(rand==0){
-    return(x)
-  } else {
-    functions<- c("de_split","deletes","transposes","replaces","insertions")
-    to_sample<- sample(1:5)
-    sampled<- sample(functions, to_sample)
-    mistakes <- apply_depth_multiple(sampled, x)
-    mistakes[['orig_word']]<- NULL
-    mistakes<- map(mistakes, ~sample(x = ., size = length(mistakes[[1]]), replace = T))
-    mistakes= unlist (mistakes)
-    mistake= sample(mistakes, size = 1)
-    if(is.null(mistake)){mistake<-'ERROR OCCURED'}
-    return(mistake)
-  }
-}
-
+# EX:
+#
+#
+#
+#
