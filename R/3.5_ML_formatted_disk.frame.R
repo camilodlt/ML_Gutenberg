8ae9cee64779d29b2f7921ca092c54af61a67eb5
Camilo De La Torre <dlt.camilo@gmail.com>
2021-06-22 00:20:35 +0200

    Try disk.frame package for larger than ram files

---
+++ R/3.5_ML_formatted_disk.frame.R
@@ -0,0 +1,22 @@
+#################################################################
+##                 3.5 : DISK FRAME converter                  ##
+#################################################################
+
+# LIBRARIES
+suppressMessages(library(disk.frame))
+suppressMessages(library(purrr))
+suppressMessages(library(dplyr))
+suppressMessages(library(feather))
+disk.frame::setup_disk.frame()
+# Common
+source("R/common.R")
+
+# FILES ------
+last_file<-last_version("data_working/", pattern = ".feather",overall_pattern = "ML_formatted")
+# READ FEATHER ------
+df<- feather::read_feather(last_file)
+# TO DISK FRAME ------
+disk_df<-as.disk.frame(df, outdir = "data_working/3.5_ML_formatted", overwrite = T, nchunks = 100)
+# RM all
+rm(list = ls())
+.rs.restartR()
